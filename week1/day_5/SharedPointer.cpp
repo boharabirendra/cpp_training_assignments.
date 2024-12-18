b2a5ef6433ea7841887783627b46e057d782bac9
boharabirendra <boharabirendra546@gmail.com>
2024-12-18 07:08:31 +0545

    Add SharedPointer

---
+++ week1/day_5/SharedPointer.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+template <typename _Tp>
+class SharedPointer
+{
+public:
+    SharedPointer(int *ptr = nullptr) : res(ptr), counter(new int(1)) {}
+
+    SharedPointer(const SharedPointer &sharePtr)
+    {
+        res = sharePtr.res;
+        counter = sharePtr.counter;
+        (*counter)++;
+    }
+
+    ~SharedPointer()
+    {
+        std::cout << "Destructor called " << std::endl;
+        (*counter)--;
+        if (*counter == 0)
+        {
+            delete res;
+        }
+    }
+
+private:
+    _Tp *res;
+    int *counter;
+};
+
+int main()
+{
+    SharedPointer<int> sharePtr(new int(100));
+    SharedPointer<int> sharePtr_1 = sharePtr;
+    SharedPointer<int> sharePtr_2 = sharePtr;
+    return 0;
+}
